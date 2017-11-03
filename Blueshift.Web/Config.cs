96f8e972e88f0dff9ae18cbff3be3477419008b2
Tom Hoag <thoag@salesforce.com>
2017-11-03 15:44:08 -0400

    Clear out quickstart garbage

--- Blueshift.Web/Config.cs
+++ Blueshift.Web/Config.cs
@@ -28,10 +28,10 @@ namespace Blueshift.Web
 					AllowedGrantTypes = GrantTypes.Implicit,
 
 					// where to redirect to after login
-					RedirectUris = { "http://localhost:5002/signin-oidc" },
+					RedirectUris = { "http://localhost:5000/signin-oidc" },
 
 					// where to redirect to after logout
-					PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+					PostLogoutRedirectUris = { "http://localhost:5000/signout-callback-oidc" },
 
 					AllowedScopes = new List<string>
 					{
